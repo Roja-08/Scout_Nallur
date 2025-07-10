d099f71b2d6adbe5c2736e36f9a1172365f21802
Keerthithev <keerthiganthevarasa@gmail.com>
2025-07-10 06:24:37 +0530

    Update CORS to allow deployed frontend domain

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5050;
 const MONGO_URI = process.env.MONGO_URI;
 
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: ['http://localhost:3000', 'https://lambent-seahorse-8eee53.netlify.app'],
   credentials: true,
 }));
 app.use(express.json());
